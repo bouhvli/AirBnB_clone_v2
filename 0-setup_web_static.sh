1325d9d5e6fb92171e1e703da63c90f324ccc704
bouhvli <bouhali.hamza.2@gmail.com>
2023-10-05 13:23:53 +0100

    push

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 # 
-if ! dpkg -l | grep -q nginx; then
-    sudo apt-get update
-    sudo apt-get install -y nginx
-fi
-sudo mkdir -p /data/web_static/releases/test/
-sudo echo "<html>
+apt-get -y update
+apt-get -y install nginx
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-sudo rm -f /data/web_static/current
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '60i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default 
-sudo service nginx restart
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+sed -i '60i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default 
+service nginx restart
