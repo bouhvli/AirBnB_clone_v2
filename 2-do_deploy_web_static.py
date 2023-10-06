a85ef951099dec85bbde35eecad2700aa637c56d
bouhvli <bouhali.hamza.2@gmail.com>
2023-10-06 17:00:22 +0100

    it's runing on my pc

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz archive"""
 from datetime import datetime
-from fabric.api import local, put, run, env
+from fabric.api import *
 from os import path
 
 
@@ -12,20 +12,17 @@ env.user = 'ubuntu'
 def do_pack():
     """
     generates a .tgz archive from the contents of
-    he web_static folder of your AirBnB Clone repo,
+    he web_static folder of your AirBnB ClonSe repo,
     using the function do_pack.
     """
-    try:
-        local('mkdir -p versions')
-        moment = datetime.now()
-        archive_file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'.format(
-            moment.year, moment.month, moment.day, moment.hour,
-            moment.minute, moment.second)
-        local('tar -cvzf {} web_static'.format(archive_file_name))
-        print('web_static packed: {} -> {}'
-              .format(archive_file_name, path.getsize(archive_file_name)))
-    except Exception:
-        return (None)
+    local('mkdir -p versions')
+    moment = datetime.now()
+    archive_file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'.format(
+        moment.year, moment.month, moment.day, moment.hour,
+        moment.minute, moment.second)
+    local('tar -cvzf {} web_static'.format(archive_file_name))
+    print('web_static packed: {} -> {}'
+          .format(archive_file_name, path.getsize(archive_file_name)))
 
 
 def do_deploy(archive_path):
@@ -35,14 +32,13 @@ def do_deploy(archive_path):
     """
     if not path.exists(archive_path):
         return (False)
-
-    get_file_name = archive_path.split('/')[-1]
-    file_path = '/data/web_static/releases/'
-    folder_path = file_path + get_file_name.split('.')[0]
     try:
+        get_file_name = archive_path.split('/')[-1]
+        file_path = '/data/web_static/releases/'
+        folder_path = file_path + get_file_name.split('.')[0]
         put(archive_path, '/tmp')
         run('mkdir -p {}/'.format(folder_path))
-        run('tar -xzf /tmp/{} -c {}'.format(get_file_name, folder_path))
+        run('tar -xzf /tmp/{} -C {}'.format(get_file_name, folder_path))
         run('rm /tmp/{}'.format(get_file_name))
         run('mv {}/web_static/* {}/'.format(folder_path, folder_path))
         run('rm -rf {}/web_static'.format(folder_path))
@@ -52,11 +48,3 @@ def do_deploy(archive_path):
         return (True)
     except Exception:
         return (False)
-
-def deploy():
-    """_summary_
-    """
-    archive_path = do_pack()
-    if not path.exists(archive_path):
-        return (False)
-    return (do_deploy(archive_path))
